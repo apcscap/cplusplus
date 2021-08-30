66d7cc5399ffaf545780ea5b4886cd7c31de9c27
calee14 <caplee1000@gmail.com>
2021-08-29 17:57:00 -0700

    learn about var type and sizes

--- hello.cpp
+++ hello.cpp
@@ -3,6 +3,17 @@ using namespace std;
 
 // main() program begins execution here
 int main() {
-    cout << "Hello World"; // print func
+    cout << "Hello World" << endl; // print func
+
+    cout << "Size of char : " << sizeof(char) << endl; // sizeof gives size of var type in bytes
+    cout << "Size of int : " << sizeof(int) << endl; // endl inserts a '\n' after every line
+    cout << "Size of short int : " << sizeof(short int) << endl;
+    cout << "Size of long int : " << sizeof(long int) << endl;
+    cout << "Size of float : " << sizeof(float) << endl;
+    cout << "Size of double : " << sizeof(double) << endl;
+    cout << "Size of wchar_t : " << sizeof(wchar_t) << endl;
+
+    
+
     return 0;
 }
