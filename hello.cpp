746236349a475c972da0e17d173d59dd0522637a
calee14 <caplee1000@gmail.com>
2021-08-29 18:06:15 -0700

    enums

--- hello.cpp
+++ hello.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main() {
     cout << "Hello World" << endl; // print func
 
-    cout << "Size of char : " << sizeof(char) << endl; // sizeof gives size of var type in bytes
+    cout << "Size of char : " << sizeof(char) << endl; // sizeof gives size of data type in bytes
     cout << "Size of int : " << sizeof(int) << endl; // endl inserts a '\n' after every line
     cout << "Size of short int : " << sizeof(short int) << endl;
     cout << "Size of long int : " << sizeof(long int) << endl;
@@ -16,6 +16,12 @@ int main() {
     typedef int feet; // can create a new type of variable with new name
     feet distance = 4;
     cout << "Distance : " << distance << endl;
+
+    enum color { red, green=5, blue } c; // enumeration type allows us to switch values based on the type name init in the {}
+    c = green;
+    cout << "Enum c is : " << c << endl; // prints out 5
+    c = blue;
+    cout << "Enum c is : " << c << endl; // prints out 6 (default prints index of enum name in {_} but will print value higher than previous as well)
 
     return 0;
 }
