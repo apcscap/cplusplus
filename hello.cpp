2b88fb4e16550c6bb38015f7b846da05d4891fca
calee14 <caplee1000@gmail.com>
2021-09-01 23:14:19 -0700

    dt lib

--- hello.cpp
+++ hello.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+#include <iostream> // input and output operations lib
 #include <ctime> // for the time
 #include <cstdlib> // probably to get the rand lib
 #include <iomanip> // 
@@ -350,6 +350,24 @@ int main() {
         cout << "vals[" << i << "] = ";
         cout << valus[i] << endl;
     }
+
+    // time references
+    // https://www.tutorialspoint.com/cplusplus/cpp_date_time.htm
+
+    
+    // current date/time based on current system
+    time_t now = time(0);
+    
+    // convert now to string form
+    char* dt = ctime(&now);
+
+    cout << "The local date and time is: " << dt << endl;   // https://stackoverflow.com/questions/17813423/cout-with-char-argument-prints-string-not-pointer-value
+                                                            // explaining how the dt char pointer will print the full string rather than the address
+
+    // convert now to tm struct for UTC
+    tm *gmtm = gmtime(&now);
+    dt = asctime(gmtm);
+    cout << "The UTC date and time is:"<< dt << endl;
 
     return 0;
 }
