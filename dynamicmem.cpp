52505ca6db274e832f31f23c2abe677992d5feb8
calee14 <caplee1000@gmail.com>
2021-09-09 23:10:28 -0700

    def how stack and dynamic mem works

---
+++ dynamicmem.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+using namespace std;
+
+// the stack - variables declared in a function will be stored in the stack for functions to use
+// the heap - free memory that will be dynamically allocated when programs run
+
