9807125cea713919681a1a46732eeeef99620bec
calee14 <caplee1000@gmail.com>
2021-09-11 00:40:01 -0700

    tired. enough c++ preprocessing for one day

--- preprocessor.cpp
+++ preprocessor.cpp
@@ -1,3 +1,10 @@
 #include <iostream>
 
 using namespace std;
+
+// preprocessors - directives that give instructions to the compiler to preprocess before compiler starts
+// we know it's a preprocessor when we see '#'
+// the #include preprocessor is for including header files into the source file (find libraries by name and put them into our session)
+
+
+
