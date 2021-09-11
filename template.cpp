1f97dab8bdc9a052ba7ca90141a1aedfd62e2519
calee14 <caplee1000@gmail.com>
2021-09-10 23:32:43 -0700

    class templates in c++

--- template.cpp
+++ template.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <cstdlib>
+#include <stdexcept>
 
 using namespace std;
 
@@ -18,6 +21,46 @@ template <typename T> T const& Max(T const& a, T const& b) {
 }
 */
 
+template <typename CT>
+class Stack {
+    private:
+        vector<CT> elems; // create a vector list of elements that will contain our CT datatype
+    public:
+        // in public we're declaring our public functions, their parameters and return types
+
+        void push(CT const&); // push an element onto the elements array
+        void pop(); // pop an element which will remove the top element
+        CT top() const; // returns the top element of typename CT
+
+        bool empty() const { // a function that returns a bool if the elems array is empty or not
+            return elems.empty();
+        }
+};
+
+template <class CT> // here we're implementing the classes outside of the class template
+void Stack<CT>::push(CT const& elem) { // properly define params
+    // append copy (reference/alias) of passed element
+    elems.push_back(elem);
+}
+
+template <class CT> 
+void Stack<CT>::pop() { // we're specifying that we're initializing the function from the 'Stack<CT>' class template
+    if(elems.empty()) {
+        throw out_of_range("Stack<>::pop(); empty stack");
+    }
+    elems.pop_back(); // remove the last element
+}
+
+template <class CT>
+CT Stack<CT>::top() const {
+    if(elems.empty()) {
+        throw out_of_range("Stack<>::pop(); empty stack");
+    }
+    return elems.back(); // returns copy of the last element
+}
+
+
+
 int main(void) {
     int i = 39;
     int j = 20;
@@ -31,5 +74,23 @@ int main(void) {
     string s2 = "World"; 
     cout << "Max(s1, s2): " << Max(s1, s2) << endl; 
     
+    try {
+        Stack<int> intStack;  // stack of ints 
+        Stack<string> stringStack;    // stack of strings 
+
+        // manipulate int stack 
+        intStack.push(7); 
+        cout << intStack.top() <<endl; 
+
+        // manipulate string stack 
+        stringStack.push("hello"); 
+        cout << stringStack.top() << std::endl; 
+        stringStack.pop(); 
+        stringStack.pop(); 
+    } catch (exception const& ex) { 
+        cerr << "Exception: " << ex.what() <<endl; 
+        return -1;
+    } 
+
     return 0;
 }
